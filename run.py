64f8ef6d07aec43f963878057eeb61e625cf340f
sivakumar <sivakumarpadala5@outlook.com>
2019-10-31 14:16:01 +0530

    added flask web pages

--- run.py
+++ run.py
@@ -3,37 +3,56 @@ import flask
 from flask import render_template, Flask, request
 
 from joblib import dump, load
+import pandas as pd
+
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
 app = Flask(__name__)
-
 
 def tokenize(text):
     print(text)
     word = re.sub(r'[^A-Za-z0-9\s]', '',text)
-    #print(word)
     words = word_tokenize(word)
-    #print(words)
     lemmatizer = WordNetLemmatizer()
     stop_words = set(stopwords.words("english"))
     words = [lemmatizer.lemmatize(w.lower(),pos="v") for w in words if w not in stop_words]
-    #print(words)
     return words
 
 model = load('chatbot.pkl')
 
+res_cols = ['query','output']
+
+df = pd.DataFrame(columns=res_cols)
+
+df.to_csv("recent_msgs.csv",sep=',')
+
 @app.route('/')
 @app.route('/index')
 def index():
-
 
     return render_template('index.html')
 
 @app.route('/go')
 def go():
     query = request.args.get('query', '')
-    print(query)
+    #print("Query",query)
 
-    return render_template('go.html')
+    pred = model.predict([query])
+    res = pred[0]
+    #print("Pred",res)
+    #print(type(res))
+
+    df1 = pd.DataFrame([query,res])
+    with open('recent_msgs.csv', 'a') as f:
+        df1.to_csv(f, header=False)
+
+    recent_msgs = df1
+
+    return render_template('go.html',query=query, predicted=res,recent_msgs=recent_msgs)
 
 app.run(host='localhost',port=3001,debug=True)
 
